47c66562070d05ea942ceb97e2304349885cd886
AuthorZhang <82961999+ZhangYanZhe20@users.noreply.github.com>
2024-04-16 21:49:04 +0800

    Create io.py

---
+++ io.py
@@ -0,0 +1,9 @@
+#输入A、B，输出A+B。
+A,B = map(int,input().split(","))
+print(A+B)
+n = int(input())
+list = list(map(int,input().split(",")))  
+print(list)
+list = input('列表:').split(",")
+list =[int(list[i]) for i in range(len(list))]
+print(list)
