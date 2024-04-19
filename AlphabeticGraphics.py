c4462d2340a0d5e04206257a9ffd66fb36bbb62b
AuthorZhang <82961999+ZhangYanZhe20@users.noreply.github.com>
2024-04-19 20:20:34 +0800

    Create AlphabeticGraphics.py

---
+++ AlphabeticGraphics.py
@@ -0,0 +1,26 @@
+"""
+利用字母可以组成一些美丽的图形，下面给出了一个例子：
+
+ABCDEFG
+
+BABCDEF
+
+CBABCDE
+
+DCBABCD
+
+EDCBABC
+
+这是一个5行7列的图形，请找出这个图形的规律，并输出一个n行m列的图形。
+"""
+n,m = map(int,input().split())
+ls = []
+for i in range(65,91):
+    ls.append(chr(i))
+str1 = "".join(ls)
+for j in range(n):
+    if j < m:
+        print(str1[j::-1],end = "")
+        print(str1[1:(m-j)])
+    else:
+        print(str1[j:(j-m):-1])
